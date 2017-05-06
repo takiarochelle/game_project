d43de90b3fcf347bcd1361c99fced16f3acd3bbd
Takia Rudolph <takiarudolph@Takias-MacBook-Pro.local>
2017-05-06 10:26:50 -0700

    Added questions to ask the user

---
+++ game_project.py
@@ -0,0 +1,22 @@
+# Creata a Buzzfeed style game.
+
+# We will guess the user's birth month based off of their coffee preferences
+
+# A one player game in which they will be asked a series of questions
+
+# Ask the user for input on these :
+    # 1. Pick a size: small, medium, large
+    # 2. Pick iced or hot: iced, hot
+    # 3. Do you add milk? y/n
+    # 4. Pick a coffee shop: Starbucks, Peets, Philz, other
+
+# Nice to Haves:
+
+# Throw an error if they choose an invalid option
+# Let the user play again
+print "Create a coffee drink and we'll guess what month you were born in!"
+
+size = raw_input("Choose a size (small, medium, or large): ")
+temp = raw_input("Choose a temp (iced or hot): ")
+drink = raw_input("Choose a drink (latte, frapp, or black coffee): ")
+milk = raw_input("Choose a milk option (2%, whole, soy, none: ")
