055606e149758190cddef1a5fa5a419033e2a140
takiarochelle <takiaroc5@gmail.com>
2017-05-22 09:14:40 -0700

    Changed layout of questions and removed the code for the emojis

--- game_project.py
+++ game_project.py
@@ -1,7 +1,6 @@
 from random import randint
 from termcolor import colored
 from results_game_project import guess_bday_fav_animal, fav_animal
-import emoji
 
 """
 
@@ -11,44 +10,52 @@ This is a one user game.
 
 """
 
+
 def choose_drink_size():
 	# If user's input is not in size_list return an error and prompt the user for input again.
 	# Return the user's value
-	size_list = ["small", "medium", "large"]
-	size = raw_input("Choose a size (small, medium, large)\n>>> ")
+	size_list = ["a", "b", "c"]
+	size = raw_input("What size do you normally get?\na. small\nb. medium\nc. large\n>>> ")
+	size = size.lower()
 	if size not in size_list:
 		print colored(size + " is not an option. Try again!", "red")
 		return choose_drink_size()
 	else:
 		return size
 
+
 def choose_pearls():
 	# If user's input is not in pearls_list return an error and prompt the user for input again. 
 	# Return the user's value
-	pearls_list = ["yes", "no"]
-	pearls = raw_input("Would you like pearls? (yes or no)\n>>> ")
+	pearls_list = ["yes", "no", "y", "n"]
+	pearls = raw_input("Would you like to add pearls? (y/n)\n>>> ")
+	pearls = pearls.lower()
 	if pearls not in pearls_list:
-		print colored(pearls + " is not an option. Try again!", "red")
+		print colored(low_pearls + " is not an option. Try again!", "red")
 		return choose_pearls()
 	else:
 		return pearls
 
+
 def choose_sweetness_level():
 	# If user's input is not in sweetness_list return an error and prompt the user for input again.
 	# Return the user's value
-	sweetness_list = ["0%", "50%", "100%"]
-	sweetness = raw_input("Pick your sugar sweetness level: (0%, 50%, 100%)\n>>> ")
+	sweetness_list = ["a", "b", "c"]
+	sweetness = raw_input("What range of sweetness do you prefer?\na. 0-50%\nb. 50-75%\nc. 75-100%\n>>> ")
+	sweetness = sweetness.lower()
 	if sweetness not in sweetness_list:
 		print colored(sweetness + " is not an option. Try again!", "red")
 		return choose_sweetness_level() 
 	else:
 		return sweetness
 
+
 def choose_flavor():
 	# If user's input is not in flavor_list return an error and propmt the user for input again.
 	# Return the user's value
-	flavor_list = ["taro", "milk tea", "oolong", "wintermelon"]
-	flavor = raw_input("Choose a flavor (taro, milk tea, oolong, wintermelon)\n>>> ")
+	flavor_list = ["a", "b", "c", "d"]
+	flavor = raw_input("Lastly, choose a flavor!\na. taro\nb. green milk tea\nc. oolong\nd. Thai milk tea\n>>> ")
+	flavor = flavor.lower()
 	if flavor not in flavor_list:
 		print colored(flavor + " is not an option. Try again!", "red")
 		return choose_flavor()
@@ -57,52 +64,53 @@ def choose_flavor():
 
 
 def play_buzzfeed_game():
-	print colored("\nCreate a boba drink and we'll guess your birthday and favorite animal!\nChoose from options given.\nLet's Play!!!", "yellow", attrs=["bold"])
+	print colored("\nCreate a boba drink and we'll guess your birthday and favorite animal!\nEnter answer after the >>>.\nLet's Play!!!", "yellow", attrs=["bold"])
 
 	size = choose_drink_size()
 	pearls = choose_pearls()
 	sweetness = choose_sweetness_level()
 	flavor = choose_flavor()
 
-	if size == "small":
- 		if pearls == "yes":
- 			print guess_bday_fav_animal("December", fav_animal) + u"\U0001F434"
- 		elif pearls == "no":
+	if size == "a":
+ 		if pearls == "yes" or pearls == "y":
+ 			print guess_bday_fav_animal("December", fav_animal) 
+ 		elif pearls == "no" or pearls == "n":
  			winter_month = randint(1, 3)
  			if winter_month == 1:
- 				print guess_bday_fav_animal("January", fav_animal) + u"\U0001F984"
+ 				print guess_bday_fav_animal("January", fav_animal) 
  			else:
- 				print guess_bday_fav_animal("February", fav_animal) + u"\U0001F427"
-	elif size == "medium":
-		if pearls == "yes":
-			if sweetness == "0%" or sweetness == "100%":
-				print guess_bday_fav_animal("October", fav_animal) + u"\U0001F418"
+ 				print guess_bday_fav_animal("February", fav_animal) 
+	elif size == "b":
+		if pearls == "yes" or pearls == "y":
+			if sweetness == "a" or sweetness == "c":
+				print guess_bday_fav_animal("October", fav_animal) 
 			else:
-				print guess_bday_fav_animal("November", fav_animal) + u"\U0001F98A"
-		elif pearls == "no":
-			print guess_bday_fav_animal("September", fav_animal) + u"\U0001F438"
-	elif size == "large":
-		if pearls == "yes":
-			if sweetness == "50%" or sweetness == "0%":
-				if flavor == "taro" or flavor == "wintermelon":
-					print guess_bday_fav_animal("March", fav_animal) + u"\U0001F42C"
+				print guess_bday_fav_animal("November", fav_animal) 
+		elif pearls == "no" or pearls == "n":
+			print guess_bday_fav_animal("September", fav_animal) 
+	elif size == "c":
+		if pearls == "yes" or pearls == "y":
+			if sweetness == "b" or sweetness == "a":
+				if flavor == "a" or flavor == "d":
+					print guess_bday_fav_animal("March", fav_animal) 
 				else:
-					print guess_bday_fav_animal("April", fav_animal) + u"\U0001F437"
+					print guess_bday_fav_animal("April", fav_animal) 
 			else:
-				print guess_bday_fav_animal("May", fav_animal) + u"\U0001F981"
-		elif pearls == "no":
-			if sweetness == "100%" or sweetness == "50%":
-				if flavor == "milk tea" or flavor == "taro":
-					print guess_bday_fav_animal("June", fav_animal) + u"\U0001F436"
-				elif flavor == "oolong" or flavor == "wintermelon":
-					print guess_bday_fav_animal("July", fav_animal) + u"\U0001F435"
+				print guess_bday_fav_animal("May", fav_animal) 
+		elif pearls == "no" or pearls == "n":
+			if sweetness == "c" or sweetness == "b":
+				if flavor == "b" or flavor == "a":
+					print guess_bday_fav_animal("June", fav_animal) 
+				elif flavor == "c" or flavor == "d":
+					print guess_bday_fav_animal("July", fav_animal) 
 			else:
-				print guess_bday_fav_animal("August", fav_animal) + u"\U0001F43C"
+				print guess_bday_fav_animal("August", fav_animal) 
 
 
 def play_again():
 	while True:
 		repeat_game = raw_input("Would you like to play again? (Type y/n): ")
+		repeat_game = repeat_game.lower()
 		if repeat_game == "y" or repeat_game == "yes":
 			play_buzzfeed_game()
 		elif repeat_game == "n" or repeat_game == "no":
